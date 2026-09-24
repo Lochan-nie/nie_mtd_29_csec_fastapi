692da22884116e0f67cfad6211521bd3ce788b79
Lochan-nie <2025cs_hnlochan_c@nie.ac.in>
2026-09-24 10:32:58 +0530

    Create main.py

---
+++ day3/main.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
+from pymongo import MongoClient
+from bson import ObjectId
+app =FastAPI()
+#db configuration
+URL="mongodb://127.0.0.1:27017"
+client=MongoClient(URL)
+db=client["service_ticket_db"]
+ticket_collection=db["tickets"]
+#schema pydantic
+class TicketCreate(BaseModel):
+    title:str
+    description:str
+    category:str
+    status:str
+class TicketResponse(TicketCreate):
+    id:str
+# helper function to convert ObjectId to string\
+def ticket_helper(ticket_doc):
+    return {
+        "id": str(ticket_doc["_id"]),
+        "title": ticket_doc["title"],
+        "description": ticket_doc["description"],
+        "category": ticket_doc["category"],
+        "status": ticket_doc["status"]
+    }
+@app.post("/tickets", status_code=201, response_model=TicketResponse)
+
+def tickect_create(payload:TicketCreate):
+    ticket_dict=payload.model_dump()#dictionary version of the object
+    result=ticket_collection.insert_one(ticket_dict)    
+    new_ticket=ticket_collection.find_one({"_id":result.inserted_id})
+    
+    return ticket_helper(new_ticket)
+@app.get("/tickets", response_model=list[TicketResponse])
+def ticket_read_all():
+    docs=ticket_collection.find()
+    tickets=[ticket_helper(doc) for doc in docs]
+    return tickets
+@app.get("/tickets/{ticket_id}", response_model=TicketResponse)
+def ticket_read_by_id(id: str):
+    
+    if not ObjectId.is_valid(id):
+        raise HTTPException(detail="Invalid ticket ID",status_code=404)
+    doc = ticket_collection.find_one({"_id": ObjectId(id)})
+    if not doc:
+        raise HTTPException(detail="Ticket not found", status_code=404)
+    return ticket_helper(doc)
+@app.put("/tickets/{ticket_id}", response_model=TicketResponse)
+def ticket_update(id: str, payload: TicketCreate):
+    if not ObjectId.is_valid(id):
+        raise HTTPException(detail="Invalid ticket ID", status_code=404)
+    ticket_dict = payload.model_dump()
+    result = ticket_collection.replace_one({"_id": ObjectId(id)}, ticket_dict)
+    if not result.matched_count:
+        raise HTTPException(detail="Ticket not found", status_code=404)
+    updated_ticket = ticket_collection.find_one({"_id": ObjectId(id)})
+    return ticket_helper(updated_ticket)
